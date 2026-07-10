e173cd9d001122a9e5309a218b528d6907c57cc9
Huy Quach <quachhuy97@gmail.com>
2026-07-10 10:36:44 +0700

    refactor(minio): slit minio resource into 3 bucket resource

--- orchestrator/resources/minio_resource.py
+++ orchestrator/resources/minio_resource.py
@@ -1,19 +1,32 @@
 # orchestration/resources/minio_resource.py
+from contextlib import asynccontextmanager
 import aioboto3
 from aiobotocore.config import AioConfig
-from dagster import ConfigurableResource
 
-class MinIOS3Resource(ConfigurableResource):
+import dagster as dg
+
+class MinIOS3Resource(dg.ConfigurableResource):
+
+    s3_bucket: str
+    s3_prefix: str = ""
+    
+
     endpoint_url: str
     aws_access_key_id: str
     aws_secret_access_key: str
+    region_name: str
 
-    def get_client(self, session: aioboto3.Session):
-        return session.client(
-            "s3",
-            endpoint_url=self.endpoint_url,
-            aws_access_key_id=self.aws_access_key_id,
-            aws_secret_access_key=self.aws_secret_access_key,
-            config=AioConfig(signature_version="s3v4"),
-            region_name="us-east-1"
-        )
+    @asynccontextmanager
+    async def get_client(self, session: aioboto3.Session):
+
+        creds_dict = {
+            "endpoint_url": self.endpoint_url,
+            "aws_access_key_id": self.aws_access_key_id,
+            "aws_secret_access_key": self.aws_secret_access_key,
+            "region_name": self.region_name
+        }
+
+        boto_config = AioConfig(signature_version="s3v4")
+        
+        async with session.client("s3", config=boto_config, **creds_dict) as client: # type: ignore
+            yield client
